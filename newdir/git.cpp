fb782beb1bc78535121a60ebd91369595e426730
deepjotkaur <deepjot.igit@gmail.com>
2020-09-12 12:02:28 +0200

    Commiting a file within a directory

---
+++ newdir/git.cpp
@@ -0,0 +1,8 @@
+mkdir gitwork
+echo "# helloworld">>README.md
+D:	 
+git add README.md
+git commit -m "First commit"
+git branch -M master
+git remote add origin https://github.com/deep-jot/helloworld.git
+git push -u origin master
